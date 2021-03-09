80ebdfb3661ae76d58704eed2f4fd06fd01fbf0c
VaughnGene <80243964+VaughnGene@users.noreply.github.com>
2021-03-09 12:19:20 +0900

    Add files via upload

---
+++ Japanese Study App/src/index.js
@@ -0,0 +1,34 @@
+const reps = document.querySelector('#desiredReps')
+const sub = document.querySelector('#calculate')
+const output = document.querySelector('.output')
+
+function time_convert(num) {
+    var hours = Math.floor(num / 60);
+    var minutes = num % 60;
+    return hours + ":" + minutes;
+}
+
+function finalCalculation() {
+    //validate numbers (okay)
+    if (reps.value == "") {
+        output.innerHTML = `<p>Please input a number</p>`
+    }
+    // calculate if numbers are okay
+    else {
+        // formula
+        let timePerRep = .442
+        let timeToFinish = reps.value * timePerRep
+
+        output.innerHTML = time_convert(timeToFinish.toFixed()) + ' time to completion'
+    }
+    
+}
+
+sub.addEventListener('click', finalCalculation)
+
+reps.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        sub.click();
+    }
+});
