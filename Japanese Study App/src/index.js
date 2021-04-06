714425d42c720af26d66e143d85e2f6dda499ede
Vaughn Moriniere <vaughnmoriniere@yahoo.com>
2021-04-06 08:17:42 -0500

    Improvments
    Reorganized the JS file to make more sense and also added some notes. Need to add more notes later for good practice.

--- Japanese Study App/src/index.js
+++ Japanese Study App/src/index.js
@@ -1,6 +1,24 @@
+//Links to the parts of the HTML document.
+
+//The input field where the reps are typed (number)
 const reps = document.querySelector('#desiredReps')
+
+//the actual button to submit the input
 const sub = document.querySelector('#calculate')
+
+//Where the output information will be display.
 const output = document.querySelector('.output')
+
+//the makes the enter key availible to use instead of clicking the buton
+reps.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        sub.click();
+    }
+});
+
+sub.addEventListener('click', finalCalculation)
+
 
 function time_convert(num) {
     var hours = Math.floor(num / 60);
@@ -23,12 +41,3 @@ function finalCalculation() {
     }
 
 }
-
-sub.addEventListener('click', finalCalculation)
-
-reps.addEventListener("keyup", function (event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        sub.click();
-    }
-});
